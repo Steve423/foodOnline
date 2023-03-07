dd85314c6c2535ea4e981498f71756996514ccd5
Steve423 <shall@uada.edu>
2023-03-07 14:23:40 -0600

    restaurant profile + google geocoding

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -156,3 +157,5 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+GOOGLE_API_KEY = 'AIzaSyAtczUBB3OFnqApgt5PueQd5d8cya0B0xk'
